bee74ee68819cfa0e745e8d1b45d3c2a7e4cd37c
Stephanie322 <gopustephaniereddy@gmail.com>
2025-10-09 12:58:02 +0530

    Signup and Login(WIP)

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,44 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest,NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+connect();
+
+export async function POST(request:NextRequest) {
+    try{
+        const reqBody=await request.json();
+        const {email,password}=reqBody;
+
+        console.log(reqBody);
+        //check user
+        const user=await User.findOne({email});
+        if(!user){
+            return NextResponse.json({error:"User does not exist"},{status:400});
+        }
+        //check password
+        const validatePassword = await bcryptjs.compare(password,user.password);
+        if(!validatePassword){
+            return NextResponse.json({error:"Invalid password"},{status:400});
+        }
+        //create token data
+        const tokenData={
+            id:user._id,
+            username:user.username,
+            email:user.email,
+        }
+        //create token
+        const token=jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" });
+        const response = NextResponse.json({
+            message:"Login succesful",
+            success:true,
+        })
+        response.cookies.set("token",token,{
+            httpOnly:true
+        });
+        return response;
+    }catch(error:any){
+        return NextResponse.json({error:error.message},{status:500});
+    }
+    
+}
